e70dac7098a9ac1dfdd4758b9890b678a67cfbd0
William Hawkins <Hawkinswill02@gmail.com>
2024-05-01 13:51:19 +0000

    Debug migrations in the database

--- blog/models.py
+++ blog/models.py
@@ -35,6 +35,7 @@ class Comment(models.Model):
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
 
+
     class Meta:
         ordering = ["created_on"]
 
